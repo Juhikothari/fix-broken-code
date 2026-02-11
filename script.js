b2c030f34c7893e77dd02658a06cb388eb16dcd2
Juhikothari <juhikthr@gmail.com>
2026-02-11 09:29:20 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increase() {
+  count = count + 1;
+  document.getElementById("count").innerText = count;
+}
+
+function decrease() {
+  count = count - 1;
+  document.getElementById("count").innerText = count;
+}
